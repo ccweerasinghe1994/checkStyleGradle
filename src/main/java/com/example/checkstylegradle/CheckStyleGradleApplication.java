16cf8bc559e6589404464fb5e0506bedd8363fea
wchamara <chamara.weerasinghe@my.sliit.lk>
2024-07-15 10:59:58 +0530

    adding checkstyle to gradle build step

---
+++ src/main/java/com/example/checkstylegradle/CheckStyleGradleApplication.java
@@ -0,0 +1,14 @@
+package com.example.checkstylegradle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckStyleGradleApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(CheckStyleGradleApplication.class, args);
+    }
+
+}
